878c909233e6d43c2053966199b5e2a3e13df648
Romain Gonord <romain.gonord.opensource@neteyes.org>
2013-11-04 11:21:52 +0100

    add demo

---
+++ demo/app/scripts/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('angularCombineViewApp', [ 'angularCombine' ]).config(function ($routeProvider) {
+	$routeProvider.when('/', {
+		templateUrl : 'views/main.html',
+		controller : 'MainCtrl'
+	}).when('/admin/entity1', {
+		templateUrl : 'views/admin/entity1.html',
+		controller : 'AdminEntity1Ctrl'
+	}).when('/admin/entity2', {
+		templateUrl : 'views/admin/entity2.html',
+		controller : 'AdminEntity2Ctrl'
+	}).when('/admin/entity3', {
+		templateUrl : 'views/admin/entity3.html',
+		controller : 'AdminEntity3Ctrl'
+	}).otherwise({
+		redirectTo : '/'
+	});
+});
+
+angular.module('angularCombineViewApp').config(function (angularCombineConfigProvider) {
+	angularCombineConfigProvider.addConf(/^views\/admin\//, 'views/admin.html');
+});
