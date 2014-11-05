a4ba61a5619863218355e525c192882ee73422f2
Romain Gonord <romain.gonord@smile.fr>
2014-11-05 17:38:06 +0100

    change remote name for bump plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -72,7 +72,8 @@ module.exports = function(grunt) {
 		bump : {
 			options : {
 				files : [ 'package.json', 'bower.json' ],
-				commitFiles : [ 'package.json', 'bower.json' ]
+				commitFiles : [ 'package.json', 'bower.json' ],
+				pushTo : 'origin'
 			}
 		}
 	});
